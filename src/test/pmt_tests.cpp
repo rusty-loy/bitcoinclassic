7b7bba6f497c65ee0cb33ef5e8b4199463576567
Ahmed Bodiwala <ahmed_bodi@msn.com>
2016-01-08 00:01:06 +0000

    Update Core Copyright in Tests
    Conflicts:
    	src/test/ReceiveMsgBytes_tests.cpp

--- src/test/pmt_tests.cpp
+++ src/test/pmt_tests.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2015-2016 The Bitcoin Classic developers
 // Copyright (c) 2012-2013 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
